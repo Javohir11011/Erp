e5855ef0ac9d5e09570b9a9d6f2279f7326d9ffe
Javohir11011 <ajavohir545@example.com>
2024-11-23 18:34:52 +0500

    .

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import express, { urlencoded } from 'express'
+import express from 'express'
 import { authRouter } from './router/index.js'
+import { createTable } from './database/tables.js'
 
 const app = express()
 
@@ -8,5 +9,9 @@ app.use(express.urlencoded({ extended: true }))
 
 // app.use("/api/v1")
 app.use('/api/v1', authRouter)
+app.get('/api/v1/setup', async (req, res) => {
+    await createTable()
+    res.send("create tables...")
+})
 
 export default app
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,12 +1,45 @@
-export const registerService = async () => {
+import connectDb from '../database/db.js'
+
+export const getByEmailService = async (email) => {
     try {
+        // console.log(email);
+        const getEmail = await connectDb
+            .select('*')
+            .from('users')
+            .where('email', email)
+        return getEmail[0]
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+export const registerService = async (user) => {
+    try {
+        const result = await getByEmailService(user.email)
+        if(result){
+            throw new Error(`Email already created...`)
+        }
+        const data =await connectDb('users')
+        .insert({
+            name:user.name,
+            email:user.email,
+            password:user.password,
+            role:user.role || 'student'
+        }).returning("*")
+        // console.log([data]);
+        return data
     } catch (error) {
         throw new Error(error)
     }
 }
 
-export const loginService = async () => {
+export const loginService = async (data) => {
     try {
+        const result = await getByEmailService(data.email)
+        // console.log(result);
+        if(!data){
+            throw new Error(error)
+        }
+        return result[0]
     } catch (error) {
         throw new Error(error)
     }
